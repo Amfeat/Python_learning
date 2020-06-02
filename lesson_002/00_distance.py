dbf1faccfe14e0ada1ae5cdca3c635ce0577d692
Amfeat <64985314+Amfeat@users.noreply.github.com>
2020-06-02 21:00:37 +0500

    init

---
+++ lesson_002/00_distance.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть словарь координат городов
+
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
+
+# Составим словарь словарей расстояний между ними
+# расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+distances = {}
+
+
+moscow_london = ((sites['Moscow'][0] - sites['London'][0])**2 + (sites['Moscow'][1] - sites['London'][1])**2)**0.5
+moscow_paris = ((sites['Moscow'][0] - sites['Paris'][0])**2 + (sites['Moscow'][1] - sites['Paris'][1])**2)**0.5
+paris_london = ((sites['Paris'][0] - sites['London'][0])**2 + (sites['Paris'][1] - sites['London'][1])**2)**0.5
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = paris_london
+distances['Paris'] = {}
+distances['Paris']['London'] = paris_london
+distances['Paris']['Moscow'] = moscow_paris
+
+
+print(distances)
+
+
+
+
