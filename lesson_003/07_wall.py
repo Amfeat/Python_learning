b511ea247ebb72afc2cfd16bf1245100904e15a3
Amfeat <64985314+Amfeat@users.noreply.github.com>
2020-06-04 23:13:19 +0500

    init

---
+++ lesson_003/07_wall.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# (цикл for)
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+
+
+# Нарисовать стену из кирпичей. Размер кирпича - 100х50
+# Использовать вложенные циклы for
+
+def brick(x, y):
+    # def rectangle(left_bottom, right_top, color=COLOR_YELLOW, width=0):
+    left_bottom = sd.get_point(x, y)
+    right_top = sd.get_point(x + 100, y + 50)
+    sd.rectangle(left_bottom, right_top, width=2)
+
+
+start_x = -25
+
+for y in range(0, 600, 50):
+    for x in range(start_x, 1200, 100):
+        brick(x, y)
+    start_x = -start_x
+
+sd.pause()
